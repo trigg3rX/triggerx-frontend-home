aae624cac8a2ff392c0fe51af12437821317f787
Lajja123 <lajja.lamprostech@gmail.com>
2025-03-11 11:25:06 +0530

    Integrated dynamic metatag

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -1,5 +1,6 @@
 import ClientSlug from "@/components/blogs page/ClientSlug";
 import { client } from "@/lib/sanity";
+import { Metadata } from "next";
 
 async function getBlog(slug) {
   const query = `*[_type == "post" && slug.current == "${slug}"][0] {
@@ -43,10 +44,38 @@ async function getBlog(slug) {
   return client.fetch(query);
 }
 
+// Generate metadata for each blog post
+export async function generateMetadata({ params }) {
+  const blog = await getBlog(params.slug);
+
+  if (!blog) {
+    return {
+      title: "Blog Not Found",
+      description: "The requested blog post could not be found.",
+    };
+  }
+
+  return {
+    title: blog.seoTitle || blog.title,
+    description: blog.metaDescription,
+    alternates: {
+      canonical: blog.canonicalUrl,
+    },
+    openGraph: {
+      title: blog.ogTitle || blog.title,
+      description: blog.ogDescription || blog.metaDescription,
+      url:
+        blog.ogUrl || `https://www.triggerx.network/blog/${blog.slug.current}`,
+      images: blog.ogImage?.asset?.url ? [{ url: blog.ogImage.asset.url }] : [],
+      type: "article",
+      publishedTime: blog.publishedAt,
+    },
+  };
+}
+
 export default async function BlogPost({ params }) {
   const blog = await getBlog(params.slug);
   console.log(blog);
 
-    return <ClientSlug blog={blog} />;
-  
+  return <ClientSlug blog={blog} />;
 }
